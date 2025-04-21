476faa96ede11722b69c755894e7d4fb6c51899f
MrMohammed <116786538+mrmohammed05@users.noreply.github.com>
2025-04-21 20:26:11 +0530

    Update1 HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-       System.out.println("hello this is the new v1");
+       System.out.println("hello this is the new v2");
     }
 }
